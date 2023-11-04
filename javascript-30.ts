a0563823779cf56862f34a76fc9b2da8a1c5d91d
Vaibhav Raj <40136017+vaibhav-raj@users.noreply.github.com>
2023-11-04 23:00:08 +0530

    Update javascript-30.ts

--- javascript-30.ts
+++ javascript-30.ts
@@ -269,32 +269,264 @@ promiseAllSettled([promise1, promise2, promise3])
 
 ================================================================================================================================================================================
 Code 17: Implement a function that returns a memoized version of a function which accepts a single argument.
+function memoize(fn) {
+  const cache = new Map();
+
+  return function (arg) {
+    if (cache.has(arg)) {
+      return cache.get(arg);
+    } else {
+      const result = fn(arg);
+      cache.set(arg, result);
+      return result;
+    }
+  };
+}
+
+// Example usage:
+
+// Function to be memoized
+function expensiveOperation(n) {
+  console.log(`Performing expensive operation for ${n}`);
+  return n * 2;
+}
+
+const memoizedExpensiveOperation = memoize(expensiveOperation);
+
+console.log(memoizedExpensiveOperation(5)); // Logs and returns 10
+console.log(memoizedExpensiveOperation(5)); // Returns 10 (result is cached)
+console.log(memoizedExpensiveOperation(10)); // Logs and returns 20
+console.log(memoizedExpensiveOperation(10)); // Returns 20 (result is cached)
+
 ================================================================================================================================================================================
 Code 18: Implement a function that formats a list of items into a single readable string.
+function formatItemList(items, delimiter = ', ', lastDelimiter = ' and ') {
+  if (items.length === 0) {
+    return '';
+  } else if (items.length === 1) {
+    return items[0];
+  } else if (items.length === 2) {
+    return items.join(lastDelimiter);
+  } else {
+    const lastItem = items.pop();
+    return items.join(delimiter) + lastDelimiter + lastItem;
+  }
+}
+
+// Example usage:
+const fruits = ['apple', 'banana', 'cherry'];
+const formattedList = formatItemList(fruits);
+
+console.log(formattedList); // Output: "apple, banana, and cherry"
+
 ================================================================================================================================================================================
 Code 19: Implement a function that converts a JavaScript value into a JSON string.
+function convertToJsonString(value) {
+  try {
+    return JSON.stringify(value);
+  } catch (error) {
+    console.error('Error converting to JSON:', error);
+    return null;
+  }
+}
+
+// Example usage:
+const data = { name: 'John', age: 30, hobbies: ['reading', 'swimming'] };
+const jsonString = convertToJsonString(data);
+
+console.log(jsonString);
 ================================================================================================================================================================================
 Code 20: Implement a class that can subscribe to and emit events that trigger attached callback functions.
 ================================================================================================================================================================================
 Code 21: Implement a debounce function that comes with a cancel method to cancel delayed invocations.
 ================================================================================================================================================================================
 Code 22: Implement a function to merge rows of data from the same user.
+function mergeUserData(data) {
+  const mergedData = {};
+
+  // Iterate through the data array
+  data.forEach((row) => {
+    const userId = row.userId; // Change this to the actual property that identifies the user
+    if (!mergedData[userId]) {
+      // If the user doesn't exist in the mergedData, create an entry
+      mergedData[userId] = { ...row };
+    } else {
+      // If the user exists, merge the data
+      Object.assign(mergedData[userId], row);
+    }
+  });
+
+  // Convert the merged data into an array
+  const mergedDataArray = Object.values(mergedData);
+
+  return mergedDataArray;
+}
+
+// Example usage:
+const data = [
+  { userId: 1, name: 'Alice', age: 25 },
+  { userId: 2, name: 'Bob', gender: 'Male' },
+  { userId: 1, job: 'Engineer' },
+];
+
+const mergedData = mergeUserData(data);
+console.log(mergedData);
+
 ================================================================================================================================================================================
 Code 23: Implement a function that recursively flattens an array into a single level deep.
+function recursiveFlatten(arr) {
+  const result = [];
+
+  arr.forEach((item) => {
+    if (Array.isArray(item)) {
+      // If the item is an array, recursively flatten it
+      result.push(...recursiveFlatten(item));
+    } else {
+      // If the item is not an array, add it to the result
+      result.push(item);
+    }
+  });
+
+  return result;
+}
+
+// Example usage:
+const nestedArray = [1, [2, 3], [4, [5, 6]]];
+const flattenedArray = recursiveFlatten(nestedArray);
+console.log(flattenedArray);
+
 ================================================================================================================================================================================
 Code 24: Implement a function that returns an object with all falsey values removed.
+function removeFalseyValues(obj) {
+  const result = {};
+
+  for (const key in obj) {
+    if (obj[key]) {
+      result[key] = obj[key];
+    }
+  }
+  return result;
+}
+// Example usage:
+const data = {
+  name: 'John',
+  age: 30,
+  email: '',
+  address: null,
+  isStudent: false,
+};
+
+const cleanedData = removeFalseyValues(data);
+console.log(cleanedData);
+
 ================================================================================================================================================================================
 Code 25: Implement a function to resolve a given value to a Promise.
+function resolveToPromise(value) {
+  return Promise.resolve(value);
+}
+
+// Example usage:
+const resolvedPromise = resolveToPromise("Resolved Value");
+
+resolvedPromise.then((result) => {
+  console.log("Resolved:", result);
+});
+
 ================================================================================================================================================================================
 Code 26: Implement a Turtle class that moves a turtle on a 2D plane.
 ================================================================================================================================================================================
 Code 27: Implement a function to execute N async tasks in series.
+async function executeTasksInSeries(tasks) {
+  for (const task of tasks) {
+    await task(); // Await the completion of the current task before moving to the next one.
+  }
+}
+
+// Example usage:
+async function task1() {
+  console.log("Task 1 started");
+  await new Promise((resolve) => setTimeout(resolve, 1000)); // Simulate an async task
+  console.log("Task 1 completed");
+}
+
+async function task2() {
+  console.log("Task 2 started");
+  await new Promise((resolve) => setTimeout(resolve, 2000)); // Simulate an async task
+  console.log("Task 2 completed");
+}
+
+async function task3() {
+  console.log("Task 3 started");
+  await new Promise((resolve) => setTimeout(resolve, 500)); // Simulate an async task
+  console.log("Task 3 completed");
+}
+
+const tasks = [task1, task2, task3];
+
+executeTasksInSeries(tasks)
+  .then(() => {
+    console.log("All tasks completed.");
+  })
+  .catch((error) => {
+    console.error("An error occurred:", error);
+  });
+
 ================================================================================================================================================================================
 Code 28: Implement a promisify function that allows the original function to override the return value.
 ================================================================================================================================================================================
 Code 29: Implement a function to deserialize a JSON string.
+function deserializeJson(jsonString) {
+  try {
+    return JSON.parse(jsonString);
+  } catch (error) {
+    console.error('Error deserializing JSON:', error);
+    return null;
+  }
+}
+
+// Example usage:
+const jsonString = '{"name": "John", "age": 30, "city": "New York"}';
+
+const deserializedObject = deserializeJson(jsonString);
+console.log(deserializedObject);
+
 ================================================================================================================================================================================
 Code 30: Implement a function to convert all the keys in an object to camel case.
+function keysToCamelCase(obj) {
+  if (typeof obj !== 'object' || obj === null) {
+    return obj; // Return as is if not an object
+  }
+
+  if (Array.isArray(obj)) {
+    return obj.map((item) => keysToCamelCase(item)); // Recursively process array items
+  }
+
+  const camelCaseObj = {};
+
+  for (const key in obj) {
+    if (obj.hasOwnProperty(key)) {
+      const camelCaseKey = key.replace(/_([a-z])/g, (_, letter) => letter.toUpperCase());
+      camelCaseObj[camelCaseKey] = keysToCamelCase(obj[key]);
+    }
+  }
+
+  return camelCaseObj;
+}
+
+// Example usage:
+const inputObject = {
+  first_name: 'John',
+  last_name: 'Doe',
+  address_info: {
+    street_name: '123 Main St',
+    postal_code: '10001',
+  },
+  hobbies: ['reading_books', 'playing_games'],
+};
+
+const camelCaseObject = keysToCamelCase(inputObject);
+console.log(camelCaseObject);
+
 ================================================================================================================================================================================
 
 
